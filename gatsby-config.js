06ae58501d7135ee266b7e595149010c8559d087
IfSince <leonlaade@gmx.de>
2023-10-26 22:33:56 +0200

    initial setup

--- gatsby-config.js
+++ gatsby-config.js
@@ -3,15 +3,21 @@
  */
 module.exports = {
   siteMetadata: {
-    title: `csr vision`,
-    siteUrl: `https://www.yourdomain.tld`
+    title: 'csr vision',
+    siteUrl: 'https://csr.vision/',
   },
-  plugins: ["gatsby-plugin-postcss", "gatsby-plugin-image", "gatsby-plugin-sharp", "gatsby-transformer-sharp", {
-    resolve: 'gatsby-source-filesystem',
-    options: {
-      "name": "images",
-      "path": "./src/images/"
+  flags: {
+    DEV_SSR: true,
+  },
+  jsxRuntime: 'automatic',
+  plugins: [
+    'gatsby-plugin-postcss', 'gatsby-plugin-image', 'gatsby-plugin-sharp', 'gatsby-transformer-sharp', {
+      resolve: 'gatsby-source-filesystem',
+      options: {
+        'name': 'images',
+        'path': './src/images/',
+      },
+      __key: 'images',
     },
-    __key: "images"
-  }]
-};
+  ],
+}
