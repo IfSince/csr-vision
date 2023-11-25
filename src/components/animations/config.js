1d98eb5811f7d3320803a3bbe57bf6a270891603
IfSince <leonlaade@gmx.de>
2023-11-25 18:59:51 +0100

    different animation for text reveal on mobile

--- src/components/animations/config.js
+++ src/components/animations/config.js
@@ -1,5 +1,5 @@
 export const DEFAULT_REVEAL_TRANSITION = {
-  duration: 0.7,
+  duration: 0.75,
   ease: [.29, .41, .33, 1],
 }  
 
--- src/components/animations/line-reveal.js
+++ src/components/animations/line-reveal.js
@@ -2,24 +2,34 @@ import { m } from 'framer-motion'
 import { DEFAULT_REVEAL_TRANSITION } from './config.js'
 
 const variants = {
-  hidden: { y: '120%' },
-  visible: { y: 0 },
+  hidden: {
+    opacity: 'var(--opacity, 0)',
+    y: 'var(--y, 0)',
+  },
+  visible: {
+    opacity: 1,
+    y: 0,
+  },
 }
 
-export const LineReveal = ({ text }) =>
-  <m.div className="flex flex-wrap"
-              initial="hidden"
-              whileInView="visible"
-              viewport={ { amount: 'all', once: true } }>
-    {
-      text?.split(' ').map((word, index) =>
-        <div style={ { clipPath: 'polygon(0% 0%, 0% 120%, 110% 120%, 110% 0%)' } } key={ index }>
-          <m.span className="block"
-                       variants={ variants }
-                       transition={ { ...DEFAULT_REVEAL_TRANSITION } }>
-            { word }&nbsp;
-          </m.span>
-        </div>,
-      )
-    }
-  </m.div>
+export const LineReveal = ({ text }) => {
+
+  return (
+    <m.div className="flex flex-wrap [--opacity:0] [--y:0] sm:[--y:120%] sm:[--opacity:1]"
+           initial="hidden"
+           whileInView="visible"
+           viewport={ { amount: 'all', once: true } }>
+      {
+        text?.split(' ').map((word, index) =>
+          <div style={ { clipPath: 'polygon(0% 0%, 0% 120%, 110% 120%, 110% 0%)' } } key={ index }>
+            <m.span className="block"
+                    variants={ variants }
+                    transition={ DEFAULT_REVEAL_TRANSITION }>
+              { word }&nbsp;
+            </m.span>
+          </div>,
+        )
+      }
+    </m.div>
+  )
+}
