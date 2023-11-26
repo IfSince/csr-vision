d7f9208fcfe69006e91d86542cde0eaea967e2cb
IfSince <leonlaade@gmx.de>
2023-11-26 23:55:12 +0100

    move attributes into attributes file

--- src/components/layout/attributes.js
+++ src/components/layout/attributes.js
@@ -1,8 +1,24 @@
 import { Attribute } from '../cards/attribute.js'
 
-export const Attributes = ({ attributes }) =>
+const ATTRIBUTES = [
+  {
+    title: 'Unternehmensimage',
+    description: 'CSR-Reports zeigen, dass ein Unternehmen sich seiner sozialen und ökologischen Verantwortung bewusst ist.',
+  },
+  {
+    title: 'Marktdifferenzierung',
+    description: 'Ein starkes Engagement für CSR kann ein Unternehmen in starken Märkten von seinen Wettbewerbern abheben.',
+  },
+  {
+    title: 'Langfristige Profitabilität',
+    description: 'Unternehmen, die CSR ernst nehmen und dies in attraktiven Berichten transparent machen, sind langfristig profitabler.',
+  },
+]
+
+
+export const Attributes = () =>
   <div className="flex w-full flex-col items-center justify-between gap-4 max-w-8xl lg:flex-row lg:items-start">
     {
-      attributes.map((attribute, index) => <Attribute { ...attribute } delay={ .15 * index } key={ attribute.title }/>)
+      ATTRIBUTES.map((attribute, index) => <Attribute { ...attribute } delay={ .15 * index } key={ attribute.title }/>)
     }
   </div>
